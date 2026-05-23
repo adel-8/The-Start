b7c31197fdacb604207638ac40156f1a6b4aa7f5
adel-8 <khiatiislam123@gmail.com>
2026-05-23 10:30:52 +0200

    fix fpm config using COPY instead of printf

--- vite.config.js
+++ vite.config.js
@@ -6,9 +6,22 @@ export default defineConfig({
         laravel({
             input: [
                 'resources/css/app.css',
-                'resources/js/app.js',
-                'resources/js/home.js',
+               
+            
                 //'resources/js/admin-analytics.js',
+                'resources/js/analytics.js',
+                'resources/js/app.js',
+                'resources/js/bootstrap.js',
+                'resources/js/cart.js',
+                'resources/js/checkout.js',
+                'resources/js/contact.js',
+                'resources/js/dashboard.js',
+                'resources/js/home.js',
+                'resources/js/navbar.js',
+                'resources/js/product.js',
+                'resources/js/Shop.js',
+                'resources/js/signin.js',
+                'resources/js/signup.js',
             ],
             refresh: true,
         }),
