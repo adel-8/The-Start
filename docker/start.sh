d3ff91800a8b6b3a52e3b6c3b108751791d8cec0
adel-8 <khiatiislam123@gmail.com>
2026-05-23 10:57:01 +0200

    fix fpm config using COPY instead of printf

--- docker/start.sh
+++ docker/start.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
 
-echo "=== PORT is: $PORT ==="
-
 php artisan config:clear || true
 php artisan migrate --force || true
+php artisan db:seed --force || true
 php artisan storage:link --force || true
 php artisan config:cache || true
 php artisan route:cache || true
 
-echo "=== Starting Laravel ==="
-php artisan serve --host=0.0.0.0 --port=$PORT
+echo "=== Starting on port 8080 ==="
+php artisan serve --host=0.0.0.0 --port=8080
