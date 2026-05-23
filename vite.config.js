d3c501afd69ae6335a67a73e654d79e3c7600568
adel-8 <khiatiislam123@gmail.com>
2026-05-23 10:26:28 +0200

    fix fpm config using COPY instead of printf

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ export default defineConfig({
             input: [
                 'resources/css/app.css',
                 'resources/js/app.js',
+                'resources/js/home.js',
                 //'resources/js/admin-analytics.js',
             ],
             refresh: true,
